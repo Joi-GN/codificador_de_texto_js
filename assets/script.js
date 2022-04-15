554d33763b3d9f25daa617bc86033e34040f775c
Joi-GN <joicegomesdn@gmail.com>
2022-04-15 01:56:36 -0300

    index,css finished

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// **Requisitos:**
+// - Deve funcionar apenas com letras minúsculas
+// - Não devem ser utilizados letras com acentos nem caracteres especiais
+// - Deve ser possível converter uma palavra para a versão criptografada e também retornar uma palavra criptografada para a versão original.
+
+// As "chaves" de criptografia que utilizaremos são:
+// `A letra "e" é convertida para "enter"`
+// `A letra "i" é convertida para "imes"`
+// `A letra "a" é convertida para "ai"`
+// `A letra "o" é convertida para "ober"`
+// `A letra "u" é convertida para "ufat"`
